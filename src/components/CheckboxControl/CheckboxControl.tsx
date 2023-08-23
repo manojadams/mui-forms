aeaeb15089ae11800d2f27a9d676129e794b3f25
Manoj <manoj.adams@gmail.com>
2023-08-23 18:18:15 +0530

    fix for #3: allow multiple value for checkbox

--- src/components/CheckboxControl/CheckboxControl.tsx
+++ src/components/CheckboxControl/CheckboxControl.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IFieldProps } from "../../common/field";
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 
@@ -7,22 +7,45 @@ interface IProps extends IFieldProps {
 }
 
 function CheckboxControl(props: IProps) {
-    const wrapperClassName = "meta-form-control-" + props.field.name;
+    const [values, setValues] = useState<string[]>([]);
+    const isNotMultple = props.form.config?.multiple === false;
+    useEffect(() => {
+        const value = props.form.value as string ?? "";
+        const values = value.split(",");
+        // if multiple config not defined, then using multiple by default
+        if (isNotMultple && values.length > 1) {
+            // do not allow multiple values
+            setValues([values[0]]);
+        } else {
+            setValues(values);
+        }
+    }, [props]);
     return (
         <FormGroup>
             {props.form.options &&
                 props.form.options.map((option: { label: string; value: any }) => {
                     return (
                         <FormControlLabel
-                            className={wrapperClassName}
+                            className={props.className}
                             key={option.value}
                             value={option.value}
                             control={
                                 <Checkbox
-                                    checked={option.value === props.form.value}
+                                    checked={values.indexOf(option.value) >= 0}
                                     onChange={(e) => {
                                         const checked = e.target.checked;
-                                        props.handleChange(e, checked ? option.value : "");
+                                        if (!isNotMultple && props.form?.options && props.form?.options.length > 0) {
+                                            const currentValue = option.value;
+                                            let currentValues = [];
+                                            if (checked) {
+                                                currentValues = [...values, currentValue];
+                                            } else {
+                                                currentValues = values.filter(val => val !== option.value);
+                                            }
+                                            props.handleChange(e, currentValues.join(","));
+                                        } else {
+                                            props.handleChange(e, checked ? option.value : "");
+                                        }
                                         props.handleValidation();
                                     }}
                                     disabled={props.form.isDisabled}
