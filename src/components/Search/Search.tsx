ccd605183bfe1627f206ba6cf8dce81309416b66
Manoj <manoj.adams@gmail.com>
2026-04-04 00:01:43 +0530

    feature-128: adding react 19 support (#129)

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -50,7 +50,8 @@ export default function Search(props: IProps) {
                 size={props.size}
                 isOptionEqualToValue={(option, value) => value && value.value === option.value}
                 onChange={(e, val) => {
-                    const actualValue = val?.value ? val.value : val?.[inputEv?.value] || "";
+                    const actualValue = val?.value ? val.value :
+                        inputEv?.value ? (val as any)?.[inputEv?.value] : "";
                     props.handleChange(e, actualValue, val !== null ? val : undefined);
                     props.handleValidation();
                     if (val) {
